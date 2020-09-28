c5417ae2f6ed1e59f9343393222808116eb52284
Magnus Bahn Berger <magn351i@edu.easj.dk>
2020-09-28 11:04:41 +0200

    Add project files.

---
+++ CsharpUnitTestAsignment1/Bicycle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace CsharpUnitTestAsignment1
+{
+    public class Bicycle
+    {
+        public uint Id { get; set; } //uint because negative Ids are rare.
+
+        private string _color;
+        private double _price;
+        private byte _gear;
+
+        public string Color
+        {
+            get { return _color; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                if (value.Length<1)
+                {
+                    throw new ArgumentException();
+                }
+                _color = value;
+            }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+                _price = value;
+            }
+        }
+
+        public byte Gear
+        {
+            get { return _gear; }
+            set
+            {
+                if (value<3 || value>32)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+                _gear = value;
+            }
+        }
+
+        public Bicycle(string color, double price, byte gear, uint id)
+        {
+            Color = color;
+            Price = price;
+            Gear = gear;
+            Id = id;
+        }
+
+        public Bicycle()
+        {
+            Color = "Not applied";
+            Gear = 3;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Color: {Color}, Price: {Price}, Gears: {Gear}";
+        }
+    }
+}
---
+++ CsharpUnitTestAsignment1Tests/BicycleTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsharpUnitTestAsignment1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpUnitTestAsignment1.Tests
+{
+    [TestClass()]
+    public class BicycleTests
+    {
+        private Bicycle _bicycle;
+
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Bicycle b1 = new Bicycle();
+            Bicycle b2 = new Bicycle("G", 11.99d, 4, 14);
+
+            Assert.AreNotEqual(null, b1);
+            Assert.AreNotEqual(null, b2);
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            _bicycle = new Bicycle();
+        }
+
+        [TestMethod]
+        public void PriceTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _bicycle.Price = -1);
+
+            _bicycle.Price = 0;
+
+            Assert.AreEqual(0d, _bicycle.Price);
+        }
+
+        [TestMethod]
+        public void ColorTest()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => _bicycle.Color = null);
+
+            Assert.ThrowsException<ArgumentException>(() => _bicycle.Color = "");
+
+            _bicycle.Color = "G";
+
+            Assert.AreEqual("G", _bicycle.Color);
+        }
+
+        [TestMethod]
+        public void GearTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _bicycle.Gear = 2);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _bicycle.Gear = 33);
+
+            _bicycle.Gear = 3;
+            
+            Assert.AreEqual(3, _bicycle.Gear);
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            Assert.AreEqual("Id: 0, Color: Not applied, Price: 0, Gears: 3", _bicycle.ToString());
+        }
+    }
+}
